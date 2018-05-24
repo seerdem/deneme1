4e27da60069c7d52c1e017aaf30c4331a0db32aa
strat <strat@strat-X555LNB>
2018-05-24 13:54:17 +0300

    deneme 2

---
+++ a/src/a/Controller.java
@@ -0,0 +1,9 @@
+package a;
+
+public class Controller {
+	public static void main(String[] args) {
+		sam s1=new sam();
+		s1.initialize();
+		System.out.println("benim adım dj dikkat");
+	}
+}
---
+++ a/src/a/sam.java
@@ -0,0 +1,33 @@
+package a;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class sam extends JFrame{
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public sam() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public void initialize() {
+		
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
